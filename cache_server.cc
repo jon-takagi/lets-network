c89384c535c90a2ad8d307f22015720fef507488
Eli <ehpoppele@gmail.com>
2020-03-23 13:36:28 -0600

    Finished all request handling except for head

--- cache_server.cc
+++ cache_server.cc
@@ -11,19 +11,6 @@
 #include <string>
 #include <iterator>
 #include "cache.hh"
-<<<<<<< HEAD
-
-struct kv_json {
-    keytype key;
-    Cache::valtype value;
-    void load(key_type k, Cache::valtype val) {
-        key = k;
-        value_ = val;
-    }
-    void write(const std::string &filename) {
-        boost::propertytree::ptree tree;
-        tree.put("key", key);
-=======
 #include "evictor.hh"
 #include <algorithm>
 #include <cstdlib>
@@ -31,6 +18,7 @@ struct kv_json {
 #include <memory>
 #include <thread>
 #include <vector>
+#include <sstream>
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
@@ -47,15 +35,11 @@ struct kv_json {
     void write(const std::string &filename) {
         boost::property_tree::ptree tree;
         tree.put("key", key_);
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
         tree.put("value", value_);
         boost::property_tree::write_json(filename, tree);
     }
 };
 
-<<<<<<< HEAD
-void process_request(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send, Cache server_cache)
-=======
 // Report a failure
 void
 fail(beast::error_code ec, char const* what)
@@ -73,17 +57,13 @@ struct bad_args_exception: public std::exception{
 template<
     class Body, class Allocator,
     class Send>
+
 void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send, Cache server_cache)
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
 {
 
     //-------------------------------------------------------
     //Some lambda nonsense for handling errors
     //Will fiugre this out later
-<<<<<<< HEAD
-
-=======
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
     // Returns a bad request response
     auto const bad_request =
     [&req](beast::string_view why)
@@ -124,64 +104,17 @@ void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req, Se
     };
     //------------------------------------
 
-<<<<<<< HEAD
     // Make sure we can handle the method
     if( req.method() != http::verb::get &&
         req.method() != http::verb::head)
         return send(bad_request("Unknown HTTP-method"));
 
-=======
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
     // Request path must be absolute and not contain "..".
     if( req.target().empty() ||
         req.target()[0] != '/' ||
         req.target().find("..") != beast::string_view::npos)
         return send(bad_request("Illegal request-target"));
 
-<<<<<<< HEAD
-    // Build the path to the requested file
-    std::string path = path_cat(doc_root, req.target());
-    if(req.target().back() == '/')
-        path.append("index.html");
-
-    // Attempt to open the file
-    beast::error_code ec;
-    http::file_body::value_type body;
-    body.open(path.c_str(), beast::file_mode::scan, ec);
-
-    // Handle the case where the file doesn't exist
-    if(ec == beast::errc::no_such_file_or_directory)
-        return send(not_found(req.target()));
-
-    // Handle an unknown error
-    if(ec)
-        return send(server_error(ec.message()));
-
-    // Cache the size since we need it after the move
-    auto const size = body.size();
-
-    // Respond to HEAD request
-    if(req.method() == http::verb::head)
-    {
-        http::response<http::empty_body> res{http::status::ok, req.version()};
-        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(http::field::content_type, mime_type(path));
-        res.content_length(size);
-        res.keep_alive(req.keep_alive());
-        return send(std::move(res));
-    }
-
-    // Respond to GET request
-    http::response<http::file_body> res{
-        std::piecewise_construct,
-        std::make_tuple(std::move(body)),
-        std::make_tuple(http::status::ok, req.version())};
-    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-    res.set(http::field::content_type, mime_type(path));
-    res.content_length(size);
-    res.keep_alive(req.keep_alive());
-    return send(std::move(res));
-=======
     // Cache the size since we need it after the move
     // auto const size = body.size();
 
@@ -206,34 +139,11 @@ void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req, Se
     // res.content_length(size);
     // res.keep_alive(req.keep_alive());
     // return send(std::move(res));
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
     //------------------------------------
     //My code
+
     if(req.method() == http::verb::get)
     {
-<<<<<<< HEAD
-      string value = std::string(req.target()).substr(1) //make a string and slice off the "/"" from the target
-      val = server_cache.get(value);
-      if(val == ""){
-          return send(not_found(req.target()));
-      } else {
-          http::response<string_body> res;
-          res.version(11);   // HTTP/1.1
-          res.result(status::ok);
-          res.set(field::server, BOOST_BEAST_VERSION_STRING);
-          kv_json json;
-          json.set(key, val);
-          json.write("output.json");
-          res.body() = "output.json";
-          res.prepare_payload();
-          res.set(http::field::content_type, "json");
-          res.content_length(value.length());
-          res.keep_alive(req.keep_alive());
-          return send(std::move(res));
-      }
-    }
-
-=======
       key_type key = std::string(req.target()).substr(1); //make a string and slice off the "/"" from the target
       Cache::size_type size;
       Cache::val_type val = server_cache.get(key, size);
@@ -244,10 +154,6 @@ void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req, Se
           res.version(11);   // HTTP/1.1
           res.result(boost::beast::http::status::ok);
           res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
-
-          // kv_json json;
-          // json.set(key, val);
-          // json.write("output.json");
           res.body() = "{ \"key\": " + key + ", \"value\":" + val + "}";
           res.set(boost::beast::http::field::content_type, "json");
           res.content_length(key.length() + size + 19);
@@ -256,36 +162,35 @@ void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req, Se
           return send(std::move(res));
       }
     }
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
+
+    //Handles a PUT request
+    //Maybe add error checking to ensure the things are of the correct types? or not necessary?
     if(req.method() == http::verb::put)
     {
-        //working on this; do later
-        //res.set(http::field::content_location, /with key here;
-        /*
-        get key and value from target
-        check if key already in Cache; remember This
-        add key to Cache
-        check for errors?
-        create a response
-        set version
-        set content_location
-        set status code based on new key or notify
-        send
-<<<<<<< HEAD
+        //First we extract the key and the value from the request target
+        std::stringstream target_string(std::string(req.target()).substr(1)); //Slice off the first "/" then make a sstream for further slicing
+        std::string key_str;
+        std::string value;
+        std::getline(target_string, key_str, '/')
+        std::getline(target_string, value, '/')
+        //And now we need to convert the value into a char pointer so we can insert into the cache
+        Cache::val_type key = const_cast<char*>(key_str.c_str())
 
-=======
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
-        */
-
-
+        //We then check if the key is already in the Cache (need for status code) and then set the value
         bool key_created = false;
-<<<<<<< HEAD
-        //insert in cache
-        http::response<string_body> res;
-=======
+        Cache::size_type size;
+        Cache::val_type val = server_cache.get(key, size);
+        if(strcmp(val, "")){
+            key_created = true;
+        }
+        server_cache.set(key, value)
+
+        //Now we can create and send the response
+        http::response<http::empty_body> res;
+        res.set(boost::beast::http::field::content_location, "/" + key_str);
         http::response<boost::beast::http::string_body> res;
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
         res.version(11);   // HTTP/1.1
+        //set the appropriate status code based on if a new entry was created
         if(key_created){
             res.result(201);
         } else {
@@ -293,88 +198,71 @@ void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req, Se
         }
         return send(std::move(res));
     }
-<<<<<<< HEAD
-=======
+
     if(req.method() == http::verb::delete_) {
-        // do delete-y stuff
+        key_type key = std::string(req.target()).substr(1);
+        auto success = server_cache.del(key);
+        http::response<boost::beast::http::string_body> res;
+        res.version(11);   // HTTP/1.1
+        res.result(boost::beast::http::status::ok);
+        res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
+        if(success){
+            res.body() = "The key " + key + " was deleted from the cache";
+            res.content_length(key.length() + 36);
+        } else {
+            res.body() = "The key " + key + " was not found in the cache";
+            res.content_length(key.length() + 36);
+        }
+        res.set(boost::beast::http::field::content_type, "text");
+        res.keep_alive(req.keep_alive());
+        res.prepare_payload();
+        return send(std::move(res));
+
     }
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
 
     //Check with Eitan on this; assignment says return only header but also wants a space-used pair which then must be in the body?
     //Also check: Accept is supposed to be a request header; also what types go there? don't we want to allow text as well?
     if(req.method() == http::verb::head)
     {
-<<<<<<< HEAD
-      http::response<string_body> res;
-      res.version(11);   // HTTP/1.1
-      res.result(status::ok);
-      res.set(field::server, BOOST_BEAST_VERSION_STRING);
-=======
-      http::response<boost::beast::http::string_body> res;
-      res.version(11);   // HTTP/1.1
-      res.result(http::status::ok);
-      res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
-      res.set(http::field::content_type, "application/json");
-      res.set(http::field::accept, "text/application/json");
-      //add the thing to the body here
-      //add the accept header
-<<<<<<< HEAD
-      res.content_length(size);
-=======
-      // res.content_length(size);
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
-      res.keep_alive(req.keep_alive());
-      return send(std::move(res));
+        http::response<boost::beast::http::string_body> res;
+        res.version(11);   // HTTP/1.1
+        res.result(http::status::ok);
+        res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "application/json");
+        res.set(http::field::accept, "text/application/json");
+        //add the thing to the body here
+        //add the accept header
+        // res.content_length(size);
+        res.keep_alive(req.keep_alive());
+        return send(std::move(res));
     }
 
-<<<<<<< HEAD
-}
-
-// Handles an HTTP server connection; based on beast example http code from boost.org
-void session(tcp::socket& socket, std::shared_ptr<std::string const> const& doc_root)
-{
-    bool close = false; //Tracking if the connection is closed
-    beast::error_code ec;
-    beast::flat_buffer buffer; //buffer for reading reqs that will persist across reads
-
-    // This lambda is used to send messages(?)
-    send_lambda<tcp::socket> lambda {socket, close, ec};
-
-    while(true)
-    {
-        // Read a request
-        http::request<http::string_body> req;
-        http::read(socket, buffer, req, ec);
-        if(ec == http::error::end_of_stream){
-            break;
-        }
-        if(ec){
-            return fail(ec, "read");
-        }
-
-        // Here we actually hand the stuff; our real work is done here
-        //first want to find out what `req` is
-        //process_request(*doc_root, std::move(req), lambda);
-
-        if(ec){
-            return fail(ec, "write");
-        }
-        if(close){
-            break;
-        }
-    }
-
-    // shutdown properly after break from close
-    socket.shutdown(tcp::socket::shutdown_send, ec);
-}
-=======
     if(req.method() == http::verb::post) {
-        //do POSTy stuff
+        if(std::string(req.target()) != "/reset"){
+            return send(not_found(req.target()));
+        }
+
+        //Resets the cache and sends back a basic response with string body
+        server_cache.reset();
+        http::response<boost::beast::http::string_body> res;
+        res.version(11);   // HTTP/1.1
+        res.result(boost::beast::http::status::ok);
+        res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.body() = "The Cache has been reset";
+        res.set(boost::beast::http::field::content_type, "text");
+        res.content_length(25);//counting null terminator
+        res.keep_alive(req.keep_alive());
+        res.prepare_payload();
+        return send(std::move(res));
     }
+
     return send(bad_request("Unknown HTTP-method"));
 
 }
+
+
+
+
 
 // Handles an HTTP server connection
 class session : public std::enable_shared_from_this<session>
@@ -602,7 +490,6 @@ private:
         do_accept();
     }
 };
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
 
 int main(int ac, char* av[])
 {
@@ -682,12 +569,4 @@ int main(int ac, char* av[])
         std::cerr << "Exception of unknown type!\n";
         return 1;
     }
-<<<<<<< HEAD
-
-    //Once we get a connection, we call the session function on it
-    //example server suggests this should be an infinite loop at the end of the try above?
-
-    return 0;
-=======
->>>>>>> ad48b6bd00872e2cffa2511b423698d60bca082c
 }
